684fe4eb56b19b06ef90c4f549e8831ed97f70eb
ivancorniel <ivancorniel30@gmail.com>
2022-03-21 22:56:10 -0400

    first commit

---
+++ coincidence.py
@@ -0,0 +1,9 @@
+class Worker:
+    def __init__(self, MO=0, TU=0, WE=0, TH=0, FR=0, SA=0, SU=0):
+        self.MO = MO
+        self.TU = TU
+        self.WE = WE
+        self.TH = TH
+        self.FR = FR
+        self.SA = SA
+        self.SU = SU
