0d2230131a7fbfa5815959648674a435647e0ed3
ivancorniel <ivancorniel30@gmail.com>
2022-03-26 08:09:47 -0400

    impoved coinciden2, evaluate instances at creation

--- coincidence2.py
+++ coincidence2.py
@@ -2,117 +2,55 @@ import sys
 from datetime import datetime
 
 
-class schedule:
+class Schedule:
 
     instances = []
+    coincidences = {}
 
     def __init__(self, worker, day, time_in, time_out):
         self.worker = worker
         self.day = day
         self.time_in = time_in
         self.time_out = time_out
-        schedule.instances.append(self)
+        Schedule.instances.append(self)
+        self.find_coincidences()
+
+    def find_coincidences(self):
+        for i in self.instances:
+            if self.day == i.day and not self.worker == i.worker:
+                if self.time_in <= i.time_out or self.time_out >= i.time_in:
+                    k = self.worker + '-' + i.worker + ':'
+                    k2 = i.worker + '-' + self.worker + ':'
+                    if k in Schedule.coincidences or k2 in Schedule.coincidences:
+                        Schedule.coincidences[k] += 1
+                    else:
+                        Schedule.coincidences.update({k : 1})
 
     @classmethod
-    def find_coincidence(cls):
-        MO, TU, WE, TH, FR, SA, SU = [], [], [], [], [], [], []
-        pairs = {}
-        for i in cls.instances:
-            if i.day == 'MO':
-                for j in MO:
-                    if i.time_in <= j.time_out or i.time_out >= j.time_in:
-                        k = i.worker + '-' + j.worker
-                        k2 = j.worker + '-' + i.worker
-                        if k in pairs or k2 in pairs:
-                            pairs[k] += 1
-                        else:
-                            pairs.update({k : 1})
-                else:    
-                    MO.append(i)
-            elif i.day == 'TU':
-                for j in TU:
-                    if i.time_in <= j.time_out or i.time_out >= j.time_in:
-                        k = i.worker + '-' + j.worker
-                        k2 = j.worker + '-' + i.worker
-                        if k in pairs or k2 in pairs:
-                            pairs[k] += 1
-                        else:
-                            pairs.update({k : 1})
-                else:    
-                    TU.append(i)
-            elif i.day == 'WE':
-                for j in WE:
-                    if i.time_in <= j.time_out or i.time_out >= j.time_in:
-                        k = i.worker + '-' + j.worker
-                        k2 = j.worker + '-' + i.worker
-                        if k in pairs or k2 in pairs:
-                            pairs[k] += 1
-                        else:
-                            pairs.update({k : 1})
-                else:    
-                    WE.append(i)                            
-            elif i.day == 'TH':
-                for j in TH:
-                    if i.time_in <= j.time_out or i.time_out >= j.time_in:
-                        k = i.worker + '-' + j.worker
-                        k2 = j.worker + '-' + i.worker
-                        if k in pairs or k2 in pairs:
-                            pairs[k] += 1
-                        else:
-                            pairs.update({k : 1})
-                else:    
-                    TH.append(i)                            
-            elif i.day == 'FR':
-                for j in FR:
-                    if i.time_in <= j.time_out or i.time_out >= j.time_in:
-                        k = i.worker + '-' + j.worker
-                        k2 = j.worker + '-' + i.worker
-                        if k in pairs or k2 in pairs:
-                            pairs[k] += 1
-                        else:
-                            pairs.update({k : 1})
-                else:    
-                    FR.append(i)                            
-            elif i.day == 'SA':
-                for j in SA:
-                    if i.time_in <= j.time_out or i.time_out >= j.time_in:
-                        k = i.worker + '-' + j.worker
-                        k2 = j.worker + '-' + i.worker
-                        if k in pairs or k2 in pairs:
-                            pairs[k] += 1
-                        else:
-                            pairs.update({k : 1})
-                else:    
-                    SA.append(i)
-            elif i.day == 'SU':
-                for j in SU:
-                    if i.time_in <= j.time_out or i.time_out >= j.time_in:
-                        k = i.worker + '-' + j.worker
-                        k2 = j.worker + '-' + i.worker
-                        if k in pairs or k2 in pairs:
-                            pairs[k] += 1
-                        else:
-                            pairs.update({k : 1})
-                else:    
-                    SU.append(i)   
-                                             
-        for k in pairs:
-            print(k, ':', pairs[k])
-                
+    def get_coincidences(cls):
+        for i in cls.coincidences:
+            print(i, cls.coincidences[i])                
 
-with open(sys.argv[1], 'r') as file:
-    schedules = file.readlines()
+    def __repr__(self) -> str:
+        return self.worker + ' ' + str(self.time_in) + ' ' + str(self.time_out)
 
-    for line in schedules:
-        sched = line.split('=')
-        sched[1] = sched[1].split(',')
-        for i in sched[1]:
-            day = i[:2]
-            time_in = datetime.strptime(i[2:7], '%H:%M').time()
-            time_out = datetime.strptime(i[8:13], '%H:%M').time()
-            key = sched[0] + day
-            key = schedule(sched[0], day, time_in, time_out)
+
+def main():
+    with open(sys.argv[1], 'r') as file:
+        schedules = file.readlines()
+
+        for line in schedules:
+            sched = line.split('=')
+            sched[1] = sched[1].split(',')
+            for i in sched[1]:
+                day = i[:2]
+                time_in = datetime.strptime(i[2:7], '%H:%M').time()
+                time_out = datetime.strptime(i[8:13], '%H:%M').time()
+                key = sched[0] + day
+                key = Schedule(sched[0], day, time_in, time_out)
+    
+    Schedule.get_coincidences()
 
 
 if __name__ == '__main__':
-    schedule.find_coincidence()
+    main()
